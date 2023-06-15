be7e57ae6f8f4842644760f8ece31547cfd68eef
Michał Plebański <michalplebanski@gmail.com>
2023-06-15 16:36:08 +0200

    add sorted objects

--- test.py
+++ test.py
@@ -8,6 +8,12 @@ class Id:
     self.job = job
     self.e_mail = e_mail
 
+  def __str__(self):
+    return f'{self.name} {self.e_mail}'
+
+  def __repr__(self):
+    return f'{self.name} {self.e_mail}'
+
 name_1 = Id(name=faker.name(), company="Widoczni", job="Expert", e_mail="mp@gmail.com")
 name_2 = Id(name=faker.name(), company="Plus", job="Leader", e_mail="mg@gmail.com")
 name_3 = Id(name=faker.name(), company="Icea", job="Senior", e_mail="mk@gmail.com")
@@ -17,4 +23,9 @@ name_5 = Id(name=faker.name(), company="Tense", job="Junior", e_mail="nn@gmail.c
 list_id = [name_1, name_2, name_3, name_4, name_5]
 
 for item in list_id:
-  print(f"{item.name} {item.e_mail}")
+  print(item)
+
+sorted_id_name = sorted(list_id, key=lambda x: x.name)
+sorted_id_email = sorted(list_id, key=lambda x: x.e_mail)
+
+print(sorted_id_email)
