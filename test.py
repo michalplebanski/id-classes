9b5df86caa8b875c8c694693ecf657d6c1c69f36
Michał Plebański <michalplebanski@gmail.com>
2023-06-15 16:43:23 +0200

    add contact() function

--- test.py
+++ test.py
@@ -9,10 +9,14 @@ class Id:
     self.e_mail = e_mail
 
   def __str__(self):
-    return f'{self.name} {self.e_mail}'
+    return f'{self.name} {self.company} {self.job} {self.e_mail}'
 
   def __repr__(self):
-    return f'{self.name} {self.e_mail}'
+    return f'{self.name} {self.company} {self.job} {self.e_mail}'
+
+  def contact(person):
+    return f"Kontaktuję się z {person}"
+
 
 name_1 = Id(name=faker.name(), company="Widoczni", job="Expert", e_mail="mp@gmail.com")
 name_2 = Id(name=faker.name(), company="Plus", job="Leader", e_mail="mg@gmail.com")
@@ -22,10 +26,9 @@ name_5 = Id(name=faker.name(), company="Tense", job="Junior", e_mail="nn@gmail.c
 
 list_id = [name_1, name_2, name_3, name_4, name_5]
 
-for item in list_id:
-  print(item)
+for person in list_id:
+  print(Id.contact(person))
 
 sorted_id_name = sorted(list_id, key=lambda x: x.name)
 sorted_id_email = sorted(list_id, key=lambda x: x.e_mail)
 
-print(sorted_id_email)
