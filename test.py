4b5975b5427dd8bc5e5da991dd4e89ca0d09f8cb
Michał Plebański <michalplebanski@gmail.com>
2023-06-15 16:19:34 +0200

    add faker package

--- test.py
+++ test.py
@@ -1,18 +1,20 @@
+from faker import Faker
+faker = Faker()
+
 class Id:
-  def __init__(self, surname, name, company, job, e_mail):
-    self.surname = surname
+  def __init__(self, name, company, job, e_mail):
     self.name = name
     self.company = company
     self.job = job
     self.e_mail = e_mail
 
-name_1 = Id(surname="Michał", name="Plebański", company="Widoczni", job="Expert", e_mail="mp@gmail.com")
-name_2 = Id(surname="Monika", name="Grabowska", company="Plus", job="Leader", e_mail="mg@gmail.com")
-name_3 = Id(surname="Mikołaj", name="Kuś", company="Icea", job="Senior", e_mail="mk@gmail.com")
-name_4 = Id(surname="Jula", name="Tychońska", company="Verseo", job="Mid", e_mail="jt@gmail.com")
-name_5 = Id(surname="Nina", name="Ninowska", company="Tense", job="Junior", e_mail="nn@gmail.com")
+name_1 = Id(name=faker.name(), company="Widoczni", job="Expert", e_mail="mp@gmail.com")
+name_2 = Id(name=faker.name(), company="Plus", job="Leader", e_mail="mg@gmail.com")
+name_3 = Id(name=faker.name(), company="Icea", job="Senior", e_mail="mk@gmail.com")
+name_4 = Id(name=faker.name(), company="Verseo", job="Mid", e_mail="jt@gmail.com")
+name_5 = Id(name=faker.name(), company="Tense", job="Junior", e_mail="nn@gmail.com")
 
 list_id = [name_1, name_2, name_3, name_4, name_5]
 
 for item in list_id:
-  print(f"{item.surname} {item.name} {item.e_mail}")
+  print(f"{item.name} {item.e_mail}")
